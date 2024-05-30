8844d68eb678c61e31a6266109d2a17199db9d65
Sooraj Rajkumar <iamsoorajrajkumar@gmail.com>
2024-05-30 14:59:17 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello welcome to the world of react js
+        
+      </h2>
+
+          </div>
+  );
+}
+
+export default App;
